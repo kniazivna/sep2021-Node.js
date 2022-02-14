1f512335163f2b4e081a027490a6b096709be0a7
kniazivna <olenkaknyaz@gmail.com>
2022-02-15 01:54:28 +0200

    test

---
+++ testLesson1/helper.js
@@ -0,0 +1,14 @@
+// function greeting (name){
+//     console.log(`Hello Node.js from ${name}`);
+// }
+// function test(){
+//     console.log("Let's start Node");
+// }
+// module.exports = {
+//     greeting,
+//     test
+// };
+
+// global.user = 'Kniazivna';
+// console.log(global.user);
+// console.log(user);
