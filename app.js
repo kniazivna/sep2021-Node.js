a0a17871aa709b755119be1ea89be11e198c8a5f
kniazivna <olenkaknyaz@gmail.com>
2022-02-15 19:19:41 +0200

    hw

--- app.js
+++ app.js
@@ -26,36 +26,77 @@ const fs = require('fs');
 // і створити файли txt в папках (online, inPerson) в яких як дату покласти юзерів з ваших масивів,
 // але щоб ваш файл виглядав як NAME: ім'я з обєкту і т.д і всі пункти з нового рядка.
 //
-const onlineUsers =[
-{name: 'Petro', age: 30, city: 'Lviv'}
-];
-onlineUsers.map(user => global.user = user);
+// const onlineUsers =[
+// {name: 'Petro', age: 30, city: 'Lviv'},
+// {name: 'Olena', age: 33, city: 'Lviv'},
+// {name: 'Oliya', age: 2, city: 'Lviv'},
+// ];
+// onlineUsers.forEach(user => {
+//     for (let key in user) {
+// fs.appendFile(path.join(__dirname, 'main','online', 'online.txt'),
+//    `\n${key.toUpperCase()}: ${user[key]}`,
+//     (err) => {
+//         if(err){
+//             console.log(err);
+//             throw err;
+//         }
+//     })}});
+//
+//
+//
+// const inPersonUsers = [
+//     {name: 'Oksana', age:31, city: 'Lviv'},
+//     {name: 'Oksana', age:31, city: 'Lviv'},
+//     {name: 'Marko', age:6, city: 'Lviv'},
+//     {name: 'Sophiya', age:4, city: 'Lviv'}
+// ];
+// inPersonUsers.map(user => {global.inUser = user;
+//     fs.appendFile(path.join(__dirname, 'main','inPerson', 'inPerson.txt'),
+//         `Name:${inUser.name}\nAge:${inUser.age}\nCity:${inUser.city}\n\n`,
+//         (err) =>{
+//             if(err){
+//                 console.log(err);
+//                 throw err;
+//             }
+//         })});
 
-const inPersonUsers = [
-    {name: 'Oksana', age:31, city: 'Lviv'}
-];
-inPersonUsers.map(user => global.inUser = user);
+//==================================================================================================================
+//Коли ви це виконаєте напишіть функцію яка буде міняти місцями юзерів з одного файлу і папки в іншу.
+// (ті, що були в папці inPerson будуть в папці online)
 
-fs.writeFile(path.join(__dirname, 'main','online', 'online.txt'),
-    `Name: ${user.name}\nAge:${user.age}\nCity:${user.city}`,
-    (err) =>{
+const onlinePath = path.join(__dirname, 'main', 'online', 'online.txt');
+const inPerson = path.join(__dirname, 'main', 'inPerson', 'inPerson.txt');
+fs.readFile(onlinePath, (err, data1) => {
     if(err){
         console.log(err);
         throw err;
     }
-})
 
-fs.writeFile(path.join(__dirname, 'main','inPerson', 'inPerson.txt'),
-    `Name:${inUser.name}\nAge:${inUser.age}\nCity:${inUser.city}`,
-    (err) =>{
+    fs.readFile(inPerson, (err,data2) =>{
         if(err){
             console.log(err);
             throw err;
         }
-    })
+        fs.writeFile(onlinePath,
+            data2,
+            (err) => {
+            if(err){
+                throw err;
+            }
+            fs.writeFile(inPerson,
+                data1,
+                (err) =>{
+                if(err){
+                    console.log(err);
+                    throw err;
+                }
+                })
+            }
+        )
 
-//==================================================================================================================
-Коли ви це виконаєте напишіть функцію яка буде міняти місцями юзерів з одного файлу і папки в іншу. (ті, що були в папці inPerson будуть в папці online)
+})})
+
+
 
 
 
