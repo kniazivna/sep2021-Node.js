264d32f99671cd3b2e7f705393ab65013968f1f2
kniazivna <olenkaknyaz@gmail.com>
2022-02-17 00:40:18 +0200

    create hbs files started app

--- app.js
+++ app.js
@@ -1,4 +1,49 @@
 const express = require('express');
 const path = require('path');
-const { engine } = require('express-handlebars');
+const {engine} = require('express-handlebars');
 
+const users = [];
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'static')));
+app.set('view engine', '.hbs');
+app.engine('.hbs', engine({defaultLayout: false}));
+app.set('views', path.join(__dirname, 'static'));
+
+
+app.listen(4000, () => {
+    console.log('SERVER HAS STARTED!!!');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+for(const id = 1; id <= users.length; id++;)
+app.post('/login', (req, res) => {
+    console.log({...req.body, id: 1});
+    // users.push(...req.body, id:`$i`);
+    // res.redirect('/users');
+});
+
+app.get('/users', (req, res) => {
+    res.render('users', {users});
+});
+
+app.get('/users/:id', (req, res) => {
+    const {id} = req.params;
+    console.log(req.query);
+    res.json(users[id]);
+});
+
+
+
+
+
+
+app.use((req, res) => {
+    res.render('notFound');
+});
