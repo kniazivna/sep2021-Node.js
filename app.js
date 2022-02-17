82ddbd831239834282914ceb085b0b525ba6e865
kniazivna <olenkaknyaz@gmail.com>
2022-02-17 15:39:12 +0200

    user id page

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require('express');
 const path = require('path');
 const {engine} = require('express-handlebars');
 
-const users = [];
-
 const app = express();
 
 app.use(express.json());
@@ -13,10 +11,8 @@ app.set('view engine', '.hbs');
 app.engine('.hbs', engine({defaultLayout: false}));
 app.set('views', path.join(__dirname, 'static'));
 
-
-app.listen(4000, () => {
-    console.log('SERVER HAS STARTED!!!');
-
+const users = [];
+let error = '';
 
 app.get('/login', (req, res) => {
     res.render('login');
@@ -27,37 +23,35 @@ app.get('/users', (req, res) => {
 });
 
 app.get('/errorPage', (req, res) => {
-    res.render('errorPage');
+    res.render('errorPage', {error});
 });
 
-for(const id = 1; id <= users.length; id++){
-app.post('/login', (req, res) => {
-    users.forEach(user => {
-        if(user.email === req.body.email){
-            res.redirect('/errorPage');
-        } else{
-            users.push({...req.body, id: `${id}`});
-            res.redirect('/users');
-        }
-    });
-});
-}
-
-app.get('/users', (req, res) => {
-    res.render('users', {users});
-});
 
 app.get('/users/:id', (req, res) => {
-    const {id} = req.params;
-    console.log(req.query);
-    res.json(users[id]);
+     const userWithId = users.find(user => user.id === +req.params.id);
+     if(!userWithId){
+         error = `USER WHITH THIS ID: ${req.params.id} WAS NOT FOUND`;
+         res.redirect('/errorPage');
+         return;
+     }
+    res.render('userById', {userWithId});
 });
 
+app.post('/login', (req, res) => {
+  const newUser = users.some(user => user.email === req.body.email);
+  if(newUser){
+      error = 'THIS EMAIL HAS USED, TRY WITH ANOTHER EMAIL';
+      res.redirect('/errorPage');
+      return;
+    console.log(req.body);
+    users.push({...req.body, id:users.length+1})
+    console.log(users);
+    res.redirect('/users');
+});
 
-
-
-
-
-// app.use((req, res) => {
-//     res.render('notFound');
-// });
+app.use((req, res) => {
+    res.render('notFound');
+});
+app.listen(4000, () => {
+    console.log('SERVER HAS STARTED!!!');
+});
