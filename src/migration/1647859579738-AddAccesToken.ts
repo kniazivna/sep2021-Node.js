d4df2aec6ed87e49e743806487ccc2b0d10eb336
kniazivna <olenkaknyaz@gmail.com>
2022-03-21 12:47:23 +0200

    hw-8-migration AddAccessToken

---
+++ src/migration/1647859579738-AddAccesToken.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class AddAccesToken1647859579738 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
