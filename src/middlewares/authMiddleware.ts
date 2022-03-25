916593f16c64f47e80643fe4d3b11c29c90a36e0
kniazivna <olenkaknyaz@gmail.com>
2022-03-25 11:33:52 +0200

    hw-8-done

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -14,19 +14,46 @@ class AuthMiddleware {
 
             const { userEmail } = tokenService.verifyToken(accessToken);
 
-            const tokenPairFromDB = await tokenRepository.findByParams({ accessToken });
-
-            if (!tokenPairFromDB) {
-                throw new Error('Token not valid');
-            }
-
             const userFromToken = await usersService.getUserByEmail(userEmail);
-
-            req.user = userFromToken;
 
             if (!userFromToken) {
                 throw new Error('Token not valid');
             }
+
+            req.user = userFromToken;
+
+            next();
+        } catch (e: any) {
+            res.json({
+                status: 400,
+                message: e.message,
+            });
+        }
+    }
+
+    public async checkRefreshToken(req: IRequestExtended, res:Response, next: NextFunction) {
+        try {
+            const refreshToken = req.get('Authorization');
+
+            if (!refreshToken) {
+                throw new Error('No token');
+            }
+
+            const { userEmail } = await tokenService.verifyToken(refreshToken, 'refresh');
+
+            const tokenPairFromDB = await tokenRepository.findByParams({ refreshToken });
+
+            if (!tokenPairFromDB) {
+                throw new Error('Wrong Token');
+            }
+
+            const userFromToken = await usersService.getUserByEmail(userEmail);
+
+            if (!userFromToken) {
+                throw new Error('Wrong Token');
+            }
+
+            req.user = userFromToken;
 
             next();
         } catch (e: any) {
