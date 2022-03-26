b5a204f3e25b88987595e67fb8629db8286259fc
kniazivna <olenkaknyaz@gmail.com>
2022-03-26 13:19:23 +0200

    lesson-9-hw-joi

--- src/validator/userValidator.ts
+++ src/validator/userValidator.ts
@@ -11,7 +11,7 @@ export const userSchema = {
             .trim()
             .required(),
         age: Joi.number().min(1).max(99).required(),
-        phone: Joi.string().pattern(/^[0-9\-\+]{9,15}$/).trim().required(),
+        phone: Joi.string().pattern(/^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[0-9]*$/).trim().required(),
         email: Joi.string().pattern(/^([a-z0-9_-]+)(@[a-z0-9-]+)(\.[a-z]+|\.[a-z]+\.[a-z]+)?$/)
             .lowercase().trim()
             .required(),
@@ -20,6 +20,10 @@ export const userSchema = {
     }),
 
     login: Joi.object().keys({
-
-    })
+        email: Joi.string().pattern(/^([a-z0-9_-]+)(@[a-z0-9-]+)(\.[a-z]+|\.[a-z]+\.[a-z]+)?$/)
+            .lowercase().trim()
+            .required(),
+        password: Joi.string().min(8).max(50).trim()
+            .required(),
+    }),
 };
