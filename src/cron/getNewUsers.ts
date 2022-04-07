d8e473983d77ef2c98252b7ada9adfaea4e1c989
kniazivna <olenkaknyaz@gmail.com>
2022-04-07 11:55:56 +0300

    hw-13-pagination+ sendMailByCronForAllUsers

--- src/cron/getNewUsers.ts
+++ src/cron/getNewUsers.ts
@@ -3,7 +3,8 @@ import { userRepository } from '../repositiries/user/userRepository';
 
 
 export const getNewUsers = async () => {
-cron.schedule('*/30 * * * * *', async () => {
+//cron.schedule('*/30 * * * * *', async () => {
+cron.schedule('30 10 24 * *', async () => {
     console.log('start work with getNewUsers');
     const newUsers = await userRepository.getNewUsers();
 
